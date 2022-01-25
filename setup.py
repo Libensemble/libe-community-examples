e6f27033fe9cffb6f7116343aa3aeaceb1fdeb19
jlnav <jnavarro@anl.gov>
2022-01-25 14:02:20 -0600

    appends to .gitignore, adds setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This is for setting up libEnsemble's community examples, details can be
+# found at https://github.com/Libensemble/libe-community-examples/
+
+from setuptools import setup
+
+
+setup(
+    name='libe-community-examples',
+    version='0.1.0',
+    description='libEnsemble community examples',
+    url='https://github.com/Libensemble/libe-community-examples',
+    author='libEnsemble developers and users',
+    author_email='libensemble@lists.mcs.anl.gov',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)

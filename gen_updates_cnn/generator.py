ed4234f97bf9f4f9d450cf4fc6d663e9000667d4
jlnav <jnavarro@anl.gov>
2025-01-16 17:10:00 -0600

    preparing a persistent generator that accepts loss and grad, and *will* (?) return new weights to reconstruct the Net on the sim's end

---
+++ gen_updates_cnn/generator.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+
+from libensemble.message_numbers import PERSIS_STOP, STOP_TAG, EVAL_GEN_TAG, WORKER_DONE
+from libensemble.specs import output_data, persistent_input_fields
+from libensemble.tools.persistent_support import PersistentSupport
+
+def create_new_weights(loss, grad):
+    # TODO: the fun stuff starts here...
+    return 0
+
+@persistent_input_fields(["loss", "grad"])
+@output_data([("weights", object)])
+def optimize_cnn(H, persis_info, gen_specs, libE_info):
+    
+    ps = PersistentSupport(persis_info, EVAL_GEN_TAG)
+    initial_complete = False
+    tag = None
+
+    while True:
+        if not initial_complete:
+            # We somehow need to send something, the sim inits itself
+            History = np.zeros(1, dtype=gen_specs["out"])
+            History["weights"] = 0  # will be disregarded by first persis sim anyway
+            initial_complete = True
+        else:
+            tag, Work, calc_in = ps.recv()
+            if tag in [PERSIS_STOP, STOP_TAG]:
+                break
+
+            loss = Work["loss"]
+            grad = Work["grad"]
+            History["weights"] = create_new_weights(loss, grad)
+        ps.send(History)
--- gen_updates_cnn/run_libe_cnn.py
+++ gen_updates_cnn/run_libe_cnn.py
@@ -1,34 +1,28 @@
 import numpy as np
 
 from libensemble import Ensemble
-from libensemble.alloc_funcs.give_pregenerated_work import give_pregenerated_sim_work
+from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens
 from libensemble.specs import ExitCriteria, AllocSpecs, SimSpecs, GenSpecs
 from simulator import mnist_training_sim
+from generator import optimize_cnn
 
 if __name__ == "__main__":
 
-    n_samples = 4
-    init_history = np.zeros(
-        n_samples, dtype=[("weights", object), ("grad", object, (10, 128)), ("loss", float), ("sim_id", int)]
-    )
-    init_history["loss"] = [0.0] * n_samples
-    init_history["sim_id"] = range(n_samples)
-
     # Create the ensemble
-    ensemble = Ensemble(parse_args=True, H0=init_history)
+    ensemble = Ensemble(parse_args=True)
 
     # Create the sim_specs
     sim_specs = SimSpecs()
     sim_specs.sim_f = mnist_training_sim
     sim_specs.inputs = ["weights"]
 
-    # gen_specs = GenSpecs()
-    # gen_specs.gen_f = "eval_cnn"
-    # gen_specs.inputs = ["acc"]
-    # gen_specs.outputs = [("weights", object)]
+    gen_specs = GenSpecs()
+    gen_specs.gen_f = optimize_cnn
+    gen_specs.inputs = ["loss", "grad"]
+    gen_specs.outputs = [("weights", object)]
 
     alloc_specs = AllocSpecs()
-    alloc_specs.alloc_f = give_pregenerated_sim_work
+    alloc_specs.alloc_f = only_persistent_gens
 
     ensemble.sim_specs = sim_specs
     # ensemble.gen_specs = gen_specs

gradients proxied and passed back to gen, where they're summed. gen trains itself using new gradients. sends updated params back to sim, but need to fix refresh not working

gen_updates_cnn/gen_updates_cnn/generator.py
@@ -15,25 +15,33 @@ from proxystore.proxy import Proxy, is_resolved
 
 from .mnist.nn import Net
 
-# grads = GET GRADIENTS and/or WEIGHTS
-# proxy = store.proxy(grads)
-
 # https://stackoverflow.com/questions/75012448/optimizer-step-not-updating-model-weights-parameters
 
-def _train(self, device, train_loader, grads, optimizer, epochs, num_networks):
-    
+def _train(model, device, train_loader, grads, optimizer, epochs):
     for param, new_grad in zip(model.parameters(), grads):
-        param.grad = torch.tensor(new_grad) / num_networks
-    
+        new_grad = new_grad.to(device)
+        param.grad = new_grad
+
     for epoch in range(epochs):
         for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
 
             optimizer.zero_grad()
-            output = self(data)
+            output = model(data)
             loss = torch.nn.CrossEntropyLoss()(output, target)
             loss.backward()
             optimizer.step()
+
+            if batch_idx % 10 == 0:
+                print(
+                    "GENERATOR: Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(train_loader.dataset),
+                        100.0 * batch_idx / len(train_loader),
+                        loss.item(),
+                    )
+                )
 
 def _connect_to_store():
     store = Store(
@@ -58,7 +66,7 @@ def _proxify_parameters(store, model):
 
 def _get_train_loader():
     from torchvision import datasets, transforms
-    train_kwargs = {"batch_size": args.batch_size}
+    train_kwargs = {"batch_size": 64}
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )
@@ -72,21 +80,25 @@ def _get_optimizer(model):
     return optimizer
 
 def _get_summed_grads(grads):
-    pass
+    summed_grads = [torch.zeros_like(i) for i in grads[0]] # base tensors for summing grads onto
+    for grad in grads:
+        for i in range(len(summed_grads)):
+            summed_grads[i] += grad[i]
+    return summed_grads
 
 @persistent_input_fields(["local_gradients"])
 @output_data([("parameters", object, (8,))])
 def network_sync(H, _, gen_specs, libE_info):
-        
+
     store = _connect_to_store()
     device = _get_device()
 
     simulators = PersistentSupport(libE_info, EVAL_GEN_TAG)
     initial_complete = False
     N = gen_specs["user"]["num_networks"]
-    
+
     model = Net().to(device)
-    model._train = _train
+    model._train = _train  # to use same Net as from nn.py, but with optimizing training routine
     train_loader = _get_train_loader()
     optimizer = _get_optimizer(model)
 
@@ -101,12 +113,12 @@ def network_sync(H, _, gen_specs, libE_info):
             if tag in [PERSIS_STOP, STOP_TAG]:
                 break
 
-            grads = calc_in["local_gradients"][0]
-            grads = [torch.from_numpy(i) for i in grads]
+            grad_proxies = calc_in["local_gradients"] # list of lists of gradient proxies
+            grads = [[torch.from_numpy(np.array(i)) for i in j] for j in grad_proxies]
             summed_grads = _get_summed_grads(grads)
-            model._train(device, train_loader, grads, optimizer, 3, N)
+            _train(model, device, train_loader, summed_grads, optimizer, 3)
             output_parameters = _proxify_parameters(store, model)
-            output["parameters"][:N] = output_parameters * N
+            output["parameters"][:N] = output_parameters * N  # since parameters for each model are same
 
         simulators.send(output)
         

gen_updates_cnn/gen_updates_cnn/mnist/nn.py
@@ -7,6 +7,7 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 import sys
 
+import numpy as np
 
 class Net(nn.Module):
     def __init__(self, parameters=None):
@@ -19,7 +20,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(128, 10)
         if parameters is not None:
             for param, value in zip(self.parameters(), parameters):
-                param.data = torch.tensor(value)
+                param.data = torch.from_numpy(np.array(value))
 
         self.total_train_loss = 0
 
@@ -39,18 +40,15 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-    def train_model(self, args, device, train_loader, optimizer, epoch):
+    def train_model(self, args, device, train_loader, epoch):
         self.train()
         for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
             output = self(data)
             loss = F.nll_loss(output, target)
-            self.total_train_loss += loss
-            optimizer.step()
             if batch_idx % args.log_interval == 0:
                 print(
-                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    "SIMULATOR: Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
                         epoch,
                         batch_idx * len(data),
                         len(train_loader.dataset),
@@ -198,13 +196,10 @@ def main(parameters=None, sim_seed=None):
         model = Net().to(device)
     else:
         model = Net(parameters).to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
-        grads = model.train_model(args, device, train_loader, optimizer, epoch)
+        grads = model.train_model(args, device, train_loader, epoch)
         model.test_model(device, test_loader)
-        scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")

gen_updates_cnn/gen_updates_cnn/simulator.py
@@ -5,13 +5,20 @@ from libensemble.tools.persistent_support import PersistentSupport as ToGenerato
 
 
 from .mnist.nn import Net, main as run_cnn
+from .generator import _connect_to_store
 
-def _run_cnn_send(generator, sim_specs, parameters):
+
+def _proxify_gradients(store, grads):
+    return [store.proxy(i.cpu().detach().numpy(), evict=True) for i in grads]
+
+
+def _run_cnn_send(generator, sim_specs, store, parameters):
 
     Output = np.zeros(1, dtype=sim_specs["out"])
 
     grads = run_cnn(parameters)
-    Output["local_gradients"] = [i.cpu().detach().numpy() for i in grads]
+
+    Output["local_gradients"] = _proxify_gradients(store, grads)
     generator.send(Output)
 
 
@@ -20,17 +27,18 @@ def _run_cnn_send(generator, sim_specs, parameters):
 @output_data(
     [("local_gradients", object, (8,))]
 )
-def mnist_training_sim(H, _, sim_specs, info):
+def mnist_training_sim(InitialData, _, sim_specs, info):
 
     generator = ToGenerator(info, EVAL_SIM_TAG)
+    store = _connect_to_store()
 
-    _run_cnn_send(generator, sim_specs, H["parameters"][0])
+    _run_cnn_send(generator, sim_specs, store, InitialData["parameters"][0])
 
     while True:
-        tag, Work, calc_in = generator.recv()
+        tag, _, SubsequentData = generator.recv()
         if tag in [PERSIS_STOP, STOP_TAG]:
             break
 
-        _run_cnn_send(generator, sim_specs, calc_in["parameters"])
+        _run_cnn_send(generator, sim_specs, store, SubsequentData["parameters"])
 
     return None, {}, 0